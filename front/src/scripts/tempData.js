af9bcf4b37c40011d5d92fa285fe9690896e80dc
estrella <angelaestrelladiaz@outlook.com>
2024-09-01 17:34:41 -0300

    estructura

---
+++ front/src/scripts/tempData.js
@@ -0,0 +1,130 @@
+const tempData = [
+    {
+        nombre: "Fullmetal alchemist",
+        genero: ["accion", "aventura", "comedia", "drama", "magia"],
+        estudio: "bones",
+        año: 2009,
+        calificacion: 8.9,
+        imagen: "../utils/fullMetal.jpg"
+    },
+    {
+        nombre: "Shingeki no kyojin",
+        genero: ["accion", "misterio", "fantasia"],
+        estudio:"mappa",
+        año: 2013,
+        calificacion: 9.2,
+        imagen: "../utils/shingeky.jpg"
+    },
+    {
+        nombre: "Hunter x hunter",
+        genero: ["accion", "aventura" , "fantasia"],
+        estudio: "viz media",
+        año: 2011,
+        calificacion: 8.4,
+        imagen: "../utils/hunter.jpg"
+    },
+    {
+        nombre: "Dragon ball",
+        genero: ["aventura", "accion" , "artes marciales"],
+        estudio: "toei",
+        año: 1986,
+        calificacion: 7.5,
+        imagen: "../utils/dragon ball.jpg"
+    },
+    {
+        nombre: "Salior moon",
+        genero: [ "magia", "romance"],
+        estudio: "toei",
+        año: 1992,
+        calificacion: 6,
+        imagen: "../utils/sailor.jpg"
+    },
+    {
+        nombre: "Naruto",
+        genero: [ "accion", "aventura" ,"artes marciales"],
+        estudio: "studio pierrot",
+        año: 2002,
+        calificacion: 7.8,
+        imagen: "../utils/naruto.jpg"
+    },
+    {
+        nombre: "Neon genesis evangelion",
+        genero: [ "accion", "ciencia ficcion", "drama"],
+        estudio: "gainax",
+        año: 1995,
+        calificacion: 8.3,
+        imagen: "../utils/neon_genesis_.jpg"
+    },
+    {
+        nombre: "Monster",
+        genero: ["drama", "thriller", "terror", "policial"],
+        estudio: "madhouse",
+        año: 2004,
+        calificacion: 8.2,
+        imagen: "../utils/monster.jpg"
+    },
+    {
+        nombre: "Boku no hero academia",
+        genero: [ "accion", "comedia", "escolar"],
+        estudio: "bones",
+        año: 2016,
+        calificacion: 8.5,
+        imagen: "../utils/hero.jpg"
+    },
+    {
+        nombre: "Slam dunk",
+        genero: ["comedia", "drama", "escolar"],
+        estudio: "bones",
+        año: 1993,
+        calificacion: 8.5,
+        imagen: "../utils/slamDunk.jpg"
+    },
+    {
+        nombre: "Death parade",
+        genero: [ "misterio", "psicologico", "drama"],
+        estudio: "madhouse",
+        año: 2015,
+        calificacion: 7.8,
+        imagen: "../utils/deathparade.jpg"
+    },
+    {
+        nombre: "Jujutsu kaisen",
+        genero: ["accion", "aventura", "fantasia"],
+        estudio: "mappa",
+        año: 2020,
+        calificacion: 9.3,
+        imagen: "../utils/jujutsu.png"
+    },
+    {
+        nombre: "Death note",
+        genero: [ "misterio", "psicologico", "thiller"],
+        estudio: "viz media",
+        año: 2006,
+        calificacion: 9.4,
+        imagen: "../utils/deathNote.jpg"
+    },
+    {
+        nombre: "Sain seiya",
+        genero: [ "accion", "fantasia", "ciencia ficcion"],
+        estudio: "toei",
+        año: 1986,
+        calificacion: 8,
+        imagen: "../utils/sainSeiya.jpg"
+    },      
+    {
+        nombre: "Hajime no ippo",
+        genero: [ "comedia", "deporte", "drama"],
+        estudio: "toei",
+        año: 2000,
+        calificacion: 8,
+        imagen: "../utils/hajime.png"
+    },  
+    {
+        nombre: "Kimetsu no yaiba",
+        genero: [ "accion", "sobrenatural"],
+        estudio: "utotable",
+        año: 2019,
+        calificacion: 9.5,
+        imagen: "../utils/kimrtsu.png"
+    }
+]
